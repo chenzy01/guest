0775500e9e3a0d4111598dd7677823f1b835b088
chenzy01 <chenzy01.hyx@gmail.com>
2019-03-30 23:48:40 +0800

    add Event table and Guest table

--- sign/admin.py
+++ sign/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from sign.models import Event, Guest
 
-# Register your models here.
+# Register your models here.让界面显示更多的字段
+class EventAdmin(admin.ModelAdmin):
+    #定义要在列表中显示哪些字段（界面上）
+    list_display = ['id', 'name', 'status', 'address', 'start_time']
+    search_fields = ['name'] #创建字段搜索栏
+    list_filter = ['status'] #创建字段过滤器
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['realname', 'phone', 'email', 'sign', 'create_time', 'event']
+    search_fields = ['realname', 'phone']
+    list_filter = ['sign']
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Guest, GuestAdmin)
