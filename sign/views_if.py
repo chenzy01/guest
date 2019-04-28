5c2de24ff6dad7e921c7c3fb79233131b04668d9
chenzy01 <chenzy01.hyx@gmail.com>
2019-04-28 23:57:44 +0800

    add guest interface

--- sign/views_if.py
+++ sign/views_if.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
-from sign.models import Event
+from sign.models import Event, Guest
+from django.db.utils import IntegrityError
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
+import time
 
 
 #添加发布会接口
@@ -72,6 +74,61 @@ def get_event_list(request):
             return JsonResponse({'status': 200, 'message': 'success', 'data': datas})
         else:
             return JsonResponse({'status': 10022, 'message': 'query result is empty'})
+
+
+#添加嘉宾接口
+def add_guest(request):
+    eid = request.POST.get('eid', '')
+    realname = request.POST.get('realname', '')
+    phone = request.POST.get('phone', '')
+    email = request.POST.get('email', '')
+
+    if eid =='' or realname == '' or phone == '':
+        return JsonResponse({'status': 10021, 'message': 'parameter error'})
+
+    result = Event.objects.filter(id=eid)
+    if not result:
+        return JsonResponse({'status': 10022, 'message': 'event id null'})
+
+    result = Event.objects.get(id=eid).status
+    if not result:
+        return JsonResponse({'status': 10023, 'message': 'event status is not available'})
+
+    event_limit = Event.objects.get(id=eid).llimit   #发布会限制人数
+    guest_limit = Guest.objects.filter(event_id=eid) #发布会已添加的嘉宾数
+
+    if len(guest_limit) >= event_limit:
+        return JsonResponse({'status': 10024, 'message': 'event number is full'})
+
+    event_time = Event.objects.get(id=eid).start_time #发布会时间
+    etime = str(event_time).split(".")[0]
+    timeArray = time.strptime(etime, "%Y-%m-%d %H:%H:%S")
+    e_time = int(time.mktime(timeArray))
+
+    now_time = str(time.time())  #当前时间
+    ntime = now_time.split(".")[0]
+    n_time = int(ntime)
+
+    if n_time >= e_time:
+        return JsonResponse({'status': 10025, 'message': 'event has started'})
+    try:
+        Guest.objects.create(realname=realname, phone=int(phone), email=email, sign=0, event_id=int(eid))
+    except IntegrityError:
+        return JsonResponse({'status': 10026, 'message': 'the event guest phone number repeat'})
+
+    return JsonResponse({'status': 200, 'message': 'add guest success'})
+
+
+#嘉宾查询接口
+
+
+
+
+
+
+
+
+
 
 
 
