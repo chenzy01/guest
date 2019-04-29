921fa5da9470ffa2090aff139b0c6539904b7686
chenzy01 <chenzy01.hyx@gmail.com>
2019-04-30 00:02:43 +0800

    add guest sign interface

--- sign/views_if.py
+++ sign/views_if.py
@@ -38,6 +38,7 @@ def add_event(request):
     return JsonResponse({'status': 200, 'message': 'add event success'})
 
 
+#查询发布会接口
 def get_event_list(request):
     eid = request.GET.get("eid", "")    #发布会 id
     name = request.GET.get("name", "")  #发布会名称
@@ -102,7 +103,7 @@ def add_guest(request):
 
     event_time = Event.objects.get(id=eid).start_time #发布会时间
     etime = str(event_time).split(".")[0]
-    timeArray = time.strptime(etime, "%Y-%m-%d %H:%H:%S")
+    timeArray = time.strptime(etime, "%Y-%m-%d %H:%M:%S")
     e_time = int(time.mktime(timeArray))
 
     now_time = str(time.time())  #当前时间
@@ -141,6 +142,52 @@ def get_guest_list(request):
             return JsonResponse({'status': 200, 'message':'success'})
         else:
             return JsonResponse({'status': 10022, 'message': 'query result is empty'})
+
+
+#发布会签到接口
+def use_sign(request):
+    eid = request.POST.get('eid', '')
+    phone = request.POST.get('phone', '')
+
+    if eid == '' or phone == '':
+        return JsonResponse({'status': 10021, 'message': 'parameter error'})
+
+    result = Event.objects.filter(id=eid)
+    if not result:
+        return JsonResponse({'status': 10022, 'message': 'event id is not null'})
+
+    result = Event.objects.get(id=eid).status
+    if not result:
+        return JsonResponse({'status': 10023, 'message': 'event status is not available'})
+
+    event_time = Event.objects.get(id=eid).start_time   #发布会时间
+    etime = str(event_time).split(".")[0]
+    timeArray = time.strptime(etime, "%Y-%m-%d %H-%M-%S")
+    e_time = int(time.mktime(timeArray))
+
+    now_time = str(time.time())    #当前时间
+    ntime = now_time.split(".")[0]
+    n_time = int(ntime)
+
+    if n_time >= e_time:
+        return JsonResponse({'status':10024, 'message': 'event has started'})
+
+    result = Guest.objects.filter(phone)
+    if not result:
+        return JsonResponse({'status': 10025, 'message': 'user phone is not null'})
+
+    result = Guest.objects.get(event_id=eid, phone=phone)
+    if not result:
+        return JsonResponse({'status': 10026, 'message': 'user did not participate in the conference'})
+
+    result = Guest.objects.get(event_id=eid, phone=phone).sign
+    if result:
+        return JsonResponse({'status': 10027, 'message': 'user has sign in'})
+    else:
+        Guest.objects.get(event_id=eid, phone=phone).update(sign=1)
+        return JsonResponse({'status': 200, 'message': 'sign in success'})
+
+
 
 
 
